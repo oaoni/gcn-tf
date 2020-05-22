5274b8cc7a9d110746e77e9f199880100f295468
Ola Oni <oa.oni7@gmail.com>
2020-05-22 00:53:11 -0400

    add general utils module

---
+++ gcn/utils/utilities.py
@@ -0,0 +1,8 @@
+"""General utilities module"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
