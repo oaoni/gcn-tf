37d13db81bbfad1235291dc63047f5885b798b7a
Ola Oni <oa.oni7@gmail.com>
2020-05-27 23:56:30 -0400

    Adding core models framework

---
+++ gcn/models/gcn.py
@@ -0,0 +1,8 @@
+"""Graph Convolutional Network based classification using Tensorflow """
+
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
