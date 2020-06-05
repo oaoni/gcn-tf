4eafa9c1822c30bf883775c1a540a974a6b65780
Ola Oni <oa.oni7@gmail.com>
2020-06-04 23:33:36 -0400

    add create model object

--- run_gcn.py
+++ run_gcn.py
@@ -1,15 +1,15 @@
+from absl import flags
+from absl import logging
+from absl import app
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
-from absl import app
-from absl import flags
 import sys
 import networkx as nx
 
-
-
+from gcn.models import gcn
 from gcn.utils import datasets
 
 #Disables AVX compile warnings
@@ -20,12 +20,19 @@ FLAGS = flags.FLAGS
 
 
 #Global configuration
-flags.DEFINE_string('dataset', 'default', 'The data set to use. ["default"]')
+flags.DEFINE_string('dataset', 'default',help='The data set to use. ["default"]')
+flags.DEFINE_string('name', 'gmu', 'Model name.')
 
-FLAGS(sys.argv)
+#GCN specific configurations
+flags.DEFINE_integer('hidden_dim', 200, 'Number of hidden units.')
+flags.DEFINE_integer('save_step', 10, 'Number of iterations to save summaries to filewriter.')
 
-if __name__ == "__main__":
 
+
+
+def main(argv):
+
+    #Load desired dataset
     if FLAGS.dataset == 'default':
 
         G = datasets.load_karate_club_graph()
@@ -37,4 +44,18 @@ if __name__ == "__main__":
         # nx.draw_circular(G, with_labels=True)
         # plt.show()
 
-    print("sfsg")
+    print(FLAGS.dataset)
+
+    #Run desired model
+    #Create the model object
+    GCN = gcn.GCN(name=FLAGS.name, hidden_dim=FLAGS.hidden_dim,
+    save_step = FLAGS.save_step)
+
+    print(GCN)
+    print("the gift that keeps giving like Babushka")
+
+
+
+if __name__ == "__main__":
+
+    app.run(main)
